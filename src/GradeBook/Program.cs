d9122e1ddb388f06acef22bcfea88279f7d855c1
opo <opo@cloudeon.com>
2021-08-19 15:39:12 +0300

    creating List

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            double result=0;
+            List<double> grades = new List<double>() {1,2,3,4};
+
+            foreach (double number in grades)
+            {
+                result += number;
+            
+            }
+            
+            Console.WriteLine(result/grades.Count);
+
+            if(args.Length>0)
+            {
+            Console.WriteLine("Hello {0}",args);
+        
+            }
+            else
+            {
+            Console.WriteLine("Hello");
+
+            }
+        }
+    }
+}
